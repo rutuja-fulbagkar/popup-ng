27aee2d8335a6eec1c5d1260c977faa656a131da
Rutuja Fulbagkar <109667489+rutuja-fulbagkar@users.noreply.github.com>
2023-06-06 21:58:04 +0530

    popup
    -6-06-2023

---
+++ home.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit  {
+  @ViewChild('exampleModal') modal!: ElementRef;
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.openModal();
+    }, 3000);
+  }
+
+  openModal() {
+    const modalElement = this.modal.nativeElement;
+    modalElement.classList.add('show');
+    modalElement.style.display = 'block';
+    modalElement.setAttribute('aria-modal', 'true');
+  }
+  closeModal(){
+    const modalElement = this.modal.nativeElement;
+    modalElement.classList.add('show');
+    modalElement.style.display = 'none';
+    modalElement.setAttribute('aria-modal', 'true');
+  }
+}
